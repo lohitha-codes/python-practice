701bd5b4ecb8035bfd242a85067c0f037204f8f0
lohitha-codes <lohithasaragada349@gmail.com>
2026-09-04 23:20:03 +0530

    Add files via upload

---
+++ Basics1/largest 0f 3 num.py
@@ -0,0 +1,9 @@
+a = int(input("enter a number: "))
+b = int(input("enter a number: "))
+c = int(input("enter a number: "))
+if(a >= b and a >= c):
+   print("a is largest")
+elif(b >= a and b >= c):
+   print("b is largest")
+else:
+   print("c is largest") 
